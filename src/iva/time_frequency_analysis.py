f4320881a12f13750a349e06a3a5dadcb6e79083
wattai <evamark.wattai@gmail.com>
2024-11-26 23:57:08 +0900

    wip: Add new stft

--- src/iva/time_frequency_analysis.py
+++ src/iva/time_frequency_analysis.py
@@ -1,9 +1,16 @@
 # -*- coding: utf-8 -*-
 
 
-def stft():
-    pass
+from scipy.signal import ShortTimeFFT
+from scipy.signal.windows import hamming
 
 
-def istft():
-    pass
+class HammingShortTimeFFT:
+    def __init__(self, win_length, hop, fs):
+        self.sft = ShortTimeFFT(hamming(win_length), hop, fs)
+
+    def stft(self, x):
+        return self.sft.stft(x)
+
+    def istft(self, s):
+        return self.sft.istft(s)
