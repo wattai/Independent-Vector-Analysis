60d146d0d1813ee3ba39a34b7b2c563dd5a3e2ed
wattai <evamark.wattai@gmail.com>
2024-11-27 00:27:58 +0900

    wip: Add tests for SFT

--- src/iva/time_frequency_analysis.py
+++ src/iva/time_frequency_analysis.py
@@ -1,16 +1,22 @@
 # -*- coding: utf-8 -*-
 
+import numpy as np
 
 from scipy.signal import ShortTimeFFT
 from scipy.signal.windows import hamming
 
 
 class HammingShortTimeFFT:
-    def __init__(self, win_length, hop, fs):
-        self.sft = ShortTimeFFT(hamming(win_length), hop, fs)
+    def __init__(self, fs: float, window_length: int, hop: int):
+        self.sft = ShortTimeFFT(hamming(window_length), hop, fs)
+        self.len_x = None
 
-    def stft(self, x):
+    def stft(self, x: np.ndarray) -> np.ndarray:
+        self.len_x = x.shape[-1]
         return self.sft.stft(x)
 
-    def istft(self, s):
-        return self.sft.istft(s)
+    def istft(self, S: np.ndarray, len_x: int | None = None) -> np.ndarray:
+        len_x = len_x or self.len_x
+        if len_x is None:
+            raise ValueError("`len_x`: length of the original signal must be provided.")
+        return self.sft.istft(S, k1=len_x)
--- tests/test_time_frequency_analysis.py
+++ tests/test_time_frequency_analysis.py
@@ -1,5 +1,28 @@
 # -*- coding: utf-8 -*-
+import pytest
+import numpy as np
+
+from iva.time_frequency_analysis import HammingShortTimeFFT
+
+SAMPLING_FREQUENCY = 16000
+WINDOW_LENGTH = 512
+HOP_LENGTH = 256
+NUM_CHANNELS = 2
 
 
-def test_stft():
-    pass
+@pytest.fixture
+def sft():
+    return HammingShortTimeFFT(SAMPLING_FREQUENCY, WINDOW_LENGTH, HOP_LENGTH)
+
+
+@pytest.mark.parametrize(
+    "x",
+    [
+        np.random.randn(SAMPLING_FREQUENCY),
+        np.random.randn(NUM_CHANNELS, SAMPLING_FREQUENCY),
+    ],
+)
+def test_sft(sft, x):
+    out = sft.istft(sft.stft(x))
+    assert out.shape == x.shape
+    assert np.allclose(out, x)
